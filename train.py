517dcfe447367d2b66fd0f3c6fbaea3fa5b4065e
Cathy Liu <56907604+cathyliu91@users.noreply.github.com>
2021-09-08 17:24:49 +0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,107 @@
+import yaml
+import os
+import torch
+torch.cuda.set_device(0)
+
+
+from torch import optim, nn
+from torch.utils.data.dataloader import DataLoader
+from model import Generator, Discriminator
+from utils import LPDataset
+
+config = yaml.load(open('config.yml'), Loader=yaml.FullLoader)
+
+node_num = config['node_num']
+window_size = config['window_size']
+subject = config['subject']
+train_data_save_path = 'data/dataset/aa/rt_hand_train_data.npy'
+train_label_save_path = 'data/dataset/aa/rt_hand_train_label.npy'
+
+train_data = LPDataset(train_data_save_path, train_label_save_path)
+sample_data = LPDataset(train_data_save_path, train_label_save_path)
+train_loader = DataLoader(
+    dataset=train_data,
+    batch_size=config['batch_size'],
+    shuffle=True,
+    pin_memory=True,
+    drop_last=True,
+)
+sample_loader = DataLoader(
+    dataset=sample_data,
+    batch_size=config['batch_size'],
+    shuffle=False,
+    pin_memory=True,
+    drop_last=True,
+)
+
+generator = Generator(
+    window_size=window_size,
+    node_num=node_num,
+    in_features=config['in_features'],
+    out_features=config['out_features'],
+    lstm_features=config['lstm_features']
+)
+
+discriminator = Discriminator(
+    input_size=node_num * node_num,
+    hidden_size=config['disc_hidden']
+)
+
+generator = generator.cuda()
+discriminator = discriminator.cuda()
+
+mse = nn.MSELoss(reduction='sum')
+
+pretrain_optimizer = optim.RMSprop(generator.parameters(), lr=config['pretrain_learning_rate'])
+generator_optimizer = optim.RMSprop(generator.parameters(), lr=config['g_learning_rate'])
+discriminator_optimizer = optim.RMSprop(discriminator.parameters(), lr=config['d_learning_rate'])
+#
+print('pretrain generator')
+
+for epoch in range(config['pretrain_epoches']):
+    for i, data in enumerate(train_loader):
+        pretrain_optimizer.zero_grad()
+        in_shots, out_shot = data
+        in_shots, out_shot = in_shots.cuda(), out_shot.cuda()
+        predicted_shot = generator(in_shots)
+        out_shot = out_shot.view(config['batch_size'], -1)
+        loss = mse(predicted_shot, out_shot)
+        loss.backward()
+        nn.utils.clip_grad_norm_(generator.parameters(), config['gradient_clip'])
+        pretrain_optimizer.step()
+        print('[epoch %d] [step %d] [loss %.4f]' % (epoch, i, loss.item()))
+
+print('train GAN')
+
+for epoch in range(config['gan_epoches']):
+    for i, (data, sample) in enumerate(zip(train_loader, sample_loader)):
+        # update discriminator
+        discriminator_optimizer.zero_grad()
+        generator_optimizer.zero_grad()
+        in_shots, out_shot = data
+        in_shots, out_shot = in_shots.cuda(), out_shot.cuda()
+        predicted_shot = generator(in_shots)
+        _, sample = sample
+        sample = sample.cuda()
+        sample = sample.view(config['batch_size'], -1)
+        real_logit = discriminator(sample).mean()
+        fake_logit = discriminator(predicted_shot).mean()
+        discriminator_loss = -real_logit + fake_logit
+        discriminator_loss.backward(retain_graph=True)
+
+        for p in discriminator.parameters():
+            p.data.clamp_(-config['weight_clip'], config['weight_clip'])
+        # update generator
+        generator_loss = -fake_logit
+        generator_loss.backward()
+
+        discriminator_optimizer.step()
+        generator_optimizer.step()
+        out_shot = out_shot.view(config['batch_size'], -1)
+        mse_loss = mse(predicted_shot, out_shot)
+        print('[epoch %d] [step %d] [d_loss %.4f] [g_loss %.4f] [mse_loss %.4f]' % (epoch, i,
+                discriminator_loss.item(), generator_loss.item(), mse_loss.item()))
+
+torch.save(generator, 'data/result/aa/rt_hand_train_data_generator.pkl')
+
+print("Finished.")
